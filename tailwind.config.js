1128d7a964103c81f4e5d0d86c7a5b24e7358308
iqbaluddin <miqbaluddin885@gmail.com>
2022-09-01 10:55:58 +0700

    rekan

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      OK: 'comic sans ms'
+    },
+    extend: {
+      lineHeight: {
+        extraloose: '2.5',
+        12: '3rem',
+        16: '4rem',
+      },
+    },
+  },
+  plugins: [],
+}
